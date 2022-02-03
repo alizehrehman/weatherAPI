7061f08edd4ba3befdff20ba750753c9d0f63f1d
Alizeh Rehman <alizehrehman26@gmail.com>
2022-02-03 14:18:39 -0800

    debugging an error with axios

--- server/helpers/getWeatherData.js
+++ server/helpers/getWeatherData.js
@@ -1,14 +1,18 @@
-const axios = require("axios");
+const axios = require('axios');
+require('dotenv').config();
 const API_KEY = process.env.API_KEY;
 
 let getWeatherData = (zipcode) => {
-    try {
-        axios.get('')
-    }
-    catch(error) {
-        console.log(error);
-        //TODO: error handling
-    }
+    axios.get(`api.openweathermap.org/data/2.5/weather?zip=${zipcode}&appid=${API_KEY}`)
+        //TODO: add ability to include country code if not in US?
+        .then(rawData => {
+            return rawData;
+        })
+        .catch(error => {
+            console.log(error);
+            // TODO: handle error handling
+        })
+    
 }
 
-module.exports = getWeatherData;
+module.exports = {getWeatherData};
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const formatWeatherData = require('./helpers/formatWeatherData.js');
-const getWeatherData = require('./helpers/getWeatherData.js');
+const { getWeatherData } = require('./helpers/getWeatherData.js');
 
 app.get('/', (req, res) => {
     res.send('Welcome to the weatherAPI service.');
